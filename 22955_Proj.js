5d1ca7136865067d0f04f59abf4bc648a23d7da2
hasaansaeed02 <hasaansaeed02@gmail.com>
2023-12-16 15:33:24 +0500

    Update 22955_Proj.js
    Borrowed Code

--- 22955_Proj.js
+++ 22955_Proj.js
@@ -1,114 +1,124 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+//SPDX-License-Identifier: GPL-3.0
 
-contract DecentralizedAuction {
-    address public owner;
-    uint public startBlock;
-    uint public endBlock;
+pragma solidity >= 0.8.0;
 
-    address public highestBidder;
-    uint public highestBindingBid;
-
-    mapping(address => uint) public bids;
-    address[] public bidders;
-
-    uint public bidIncrement;
-
-    uint public constant min_bidlength = 3;
-    uint public constant min_fintime = 120;
-
-    uint public biddercount;
-
-    constructor(uint _startBlock, uint _endBlock, uint _bidIncrement) {
-        require(_startBlock < _endBlock, "Invalid endBlock");
-        require(_bidIncrement > 0, "Bid increment must be greater than zero");
-
-        owner = msg.sender;
-        startBlock = _startBlock;
-        endBlock = _endBlock;
-        bidIncrement = _bidIncrement;
-    }
-
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Only the owner can call this function");
-        _;
-    }
-
-    modifier onlyBeforeEnd() {
-        require(block.timestamp < endBlock, "Auction has already ended");
-        _;
-    }
-
-    modifier onlyAfterEnd() {
-        require(block.timestamp > endBlock, "Auction has not ended yet");
-        _;
-    }
-
-    modifier onlyNotOwner() {
-        require(msg.sender != owner, "Owner cannot bid on their own auction");
-        _;
-    }
-
-    modifier canFinalize() {
-        require(biddercount >= min_bidlength, "Can finalize when atleast 3 bidders bid");
-        require(block.timestamp > endBlock + min_fintime, "Can finalize when atleast 2 minutes passed");
-        _;
-    }
+ contract auction {
+    address payable  owner;
+    address highestBidder;
+    uint256 startingTime;
+    uint256 endingTime;
+    event LogUint(uint number);
+    mapping(address => uint256) public bids;
+    uint highestBindingBid;
+    address[] public bidKeys;
+    
 
 
-    function placeBid() external payable onlyBeforeEnd onlyNotOwner {
-        require(msg.value > bids[msg.sender] + bidIncrement, "Bid must be greater than the previous highest bid plus the bid increment");
+    
+    function placeBids() payable  external {
+    
+        require(block.timestamp >= startingTime, "Auction has not started");
+        require(block.timestamp < endingTime, "Auction has ended");
+        require(msg.value > highestBindingBid, "Bid is not high enough");
 
-        if (bids[msg.sender] == 0) {
-            // If this is the first bid from the bidder, add them to the bidders array
-            bidders.push(msg.sender);
-            biddercount++; // Increment bidder count
-        }
 
-        if (bids[msg.sender] > 0) {
-            // Refund the previous bid
-            payable(msg.sender).transfer(bids[msg.sender]);
-        }
 
-        bids[msg.sender] = msg.value;
+        bool check = false;
 
         if (msg.value > bids[highestBidder]) {
+
+             for (uint i = 0; i < bidKeys.length; i++) {
+                if (bidKeys[i] == msg.sender) {
+                check = true;
+                break;
+            }
+        }
+        if(check == true){
             highestBidder = msg.sender;
-            highestBindingBid = bids[msg.sender] + bidIncrement;
+            bids[msg.sender] = msg.value;
+            highestBindingBid = msg.value;
         }
-    }
-
-    function finalizeAuction() external onlyOwner onlyAfterEnd canFinalize {
-        require(highestBidder != address(0), "Auction has no winner");
-
-        // Transfer the funds to the owner
-        payable(owner).transfer(highestBindingBid);
-
-        // Transfer any excess funds back to the highest bidder
-        if (bids[highestBidder] > highestBindingBid) {
-            payable(highestBidder).transfer(bids[highestBidder] - highestBindingBid);
+        else{
+            bidKeys.push(msg.sender);
+            highestBidder = msg.sender;
+            bids[msg.sender] = msg.value;
+            highestBindingBid = bids[highestBidder];
         }
-    }
 
-    function cancelAuction() external onlyOwner onlyBeforeEnd {
-        // Refund all bidders
-        for (uint i = 0; i < bidders.length; i++) {
-            address bidder = bidders[i];
-            uint amount = bids[bidder];
             
-            // Refund the bidder
-            payable(bidder).transfer(amount);
         }
-        endBlock = block.number; // Set the end block to the current block, effectively ending the auction
+    }
+    
+
+    function currentHigestBid() public  view returns (uint256) {
+        return highestBindingBid;
+
     }
 
-    // Allow bidders to withdraw their funds after the auction ends
-    function withdraw() external {
-        require(block.number > endBlock, "Auction has not ended yet");
-        require(msg.sender != highestBidder, "Highest bidder cannot withdraw until the auction is finalized");
 
-        uint amount = bids[msg.sender];
-        bids[msg.sender] = 0;
-        payable(msg.sender).transfer(amount);
+   function setStartTime(uint time) private  OnlyOwner{
+        startingTime = time;
+   }
+
+
+   function setEndTime(uint time) private  OnlyOwner{
+        endingTime = time;
+   }
+    constructor(uint _startTime, uint _endTime) public  {
+            owner = payable(msg.sender);
+            setEndTime(_endTime);
+            setStartTime(_startTime);
+         
+        }
+
+
+
+
+
+   modifier  OnlyOwner() {
+        require(msg.sender == owner);
+        //require(block.timestamp > startingTime);
+    
+    _;
+   }
+
+
+
+  function cancelAuction() external payable  OnlyOwner {
+        require(block.timestamp < endingTime, "Auction already ended");
+     
+        for (uint i = 0; i < bidKeys.length; i++) {
+        address payable bidder = payable(bidKeys[i]);
+        uint256 amount = bids[bidder];
+        
+
+        emit LogUint(amount);
+
+        if (amount > 0) {
+            bids[bidder] = 0;
+            bidder.transfer(amount);
+        }
     }
-}
+
+        selfdestruct(owner);
+    }  
+ function finalizeAuction() external payable  OnlyOwner {
+        require(block.timestamp >= endingTime, "Auction not yet ended");
+        
+        // Refund the remaining bids to the bidders
+     
+           for (uint i = 0; i < bidKeys.length; i++) {
+            address payable bidder = payable(bidKeys[i]);
+            uint amount = bids[bidder];
+            if ((amount > 0) && (bidder != highestBidder)) {
+                bids[bidder] = 0;
+                bidder.transfer(amount);
+                
+
+            }
+        }
+     
+        selfdestruct(owner);
+    }
+
+ }
